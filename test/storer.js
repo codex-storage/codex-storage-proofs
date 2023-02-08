bad2a2f746730ab77c83e6481826b398e6c9cc77
Dmitriy Ryajov <dryajov@gmail.com>
2023-02-08 14:15:16 -0600

    add failing case

--- test/storer.js
+++ test/storer.js
@@ -7,11 +7,15 @@ const mimc7 = require("circomlibjs").mimc7;
 const mimcsponge = require("circomlibjs").mimcsponge;
 const { MerkleTree } = require("merkletreejs");
 const {c} = require("circom_tester");
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
 
 exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 const Fr = new F1Field(exports.p);
 
 const assert = chai.assert;
+const expect = chai.expect;
 
 const wasm_tester = require("circom_tester").wasm;
 const key = BigInt(2);
@@ -20,6 +24,8 @@ const digest = (buf) => mimc7.hash(buf, key);
 const digestMulti = (buf) => mimc7.multiHash(buf, key);
 
 function merkelize(leafs) {
+  // simple merkle root (treehash) generator
+  // unbalanced trees will have the last leaf duplicated
   var merkle = leafs;
 
   while (merkle.length > 1) {
@@ -62,15 +68,15 @@ describe("Storer test", function () {
     assert.equal(hash, root);
   });
 
-  it("Should hash correctly", async () => {
+  it("Should verify chunk is correct and part of dataset", async () => {
     const cir = await wasm_tester(path.join(__dirname, "./circuits", "storer_test.circom"));
 
-    let root = merkelize([aHash, bHash, cHash, dHash]);
+    const root = merkelize([aHash, bHash, cHash, dHash]);
 
     const parentHashL = digestMulti([aHash, bHash]);
     const parentHashR = digestMulti([cHash, dHash]);
 
-    const witness = await cir.calculateWitness({
+    await cir.calculateWitness({
       "chunks": [[a], [b], [c], [d]],
       "siblings": [[bHash, parentHashR], [aHash, parentHashR], [dHash, parentHashL], [cHash, parentHashL]],
       "hashes": [aHash, bHash, cHash, dHash],
@@ -80,4 +86,28 @@ describe("Storer test", function () {
     }, true);
   }).timeout(100000);
 
+  it("Should verify chunk is correct and part of dataset", async () => {
+    const cir = await wasm_tester(path.join(__dirname, "./circuits", "storer_test.circom"));
+
+    const root = merkelize([aHash, bHash, cHash, dHash]);
+
+    const parentHashL = digestMulti([aHash, bHash]);
+    const parentHashR = digestMulti([cHash, dHash]);
+
+    const fn = async () => {
+      return await cir.calculateWitness({
+        "chunks": [[salt], [b], [c], [d]],
+        "siblings": [[bHash, parentHashR], [aHash, parentHashR], [dHash, parentHashL], [cHash, parentHashL]],
+        "hashes": [saltHash, bHash, cHash, dHash],
+        "path": [0, 1, 2, 3],
+        "root": root,
+        "salt": saltHash,
+      }, true);
+    }
+
+    assert.isRejected(
+      fn(), Error,
+      /Error: Error: Assert Failed.\nError in template StorageProver_7 line: 75/);
+
+  }).timeout(100000);
 });
