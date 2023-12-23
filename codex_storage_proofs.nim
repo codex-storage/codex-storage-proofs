44d8e3a22c8b1a29736353e37ae3f7f61d587c27
Jaremy Creechley <creechley@gmail.com>
2023-12-22 18:07:17 -0700

    initial setup

--- codex_storage_proofs.nim
+++ codex_storage_proofs.nim
@@ -1,11 +1,22 @@
 
 import std/os
 
+const
+  currentDir = currentSourcePath().parentDir()
+  lib_path* = currentDir/"target"/"release"/"libcodex_storage_proofs.a"
+
 static:
-  let pth = currentSourcePath().parentDir()
-  echo "\n===="
-  echo "CODEX:STORAGE:PROOFS: ", pth
+  echo "\n==== CODEX:STORAGE:PROOFS: "
+  echo "cwd: ", currentDir
+  echo "lib_path: ", lib_path
   # echo "pwd: ", projectDir()
   # echo "cwd: ", getCurrentDir()
+  let cmd = "pwd && cargo build --release"
+  echo "\nBuilding codex-storage-proofs: " & cmd
+  let (output, exitCode) = gorgeEx cmd
+  echo output
+  if exitCode != 0:
+    # discard gorge "rm -rf " & buildDir
+    raise (ref Defect)(msg: "Failed to build codex-storage-proofs")
 
   echo "\n===="
