c404233d8aa54a98d5a299eaeb6dbca7be0d14ea
Jaremy Creechley <creechley@gmail.com>
2024-01-10 16:30:18 +0200

    fix stuffs

--- build.nims
+++ build.nims
@@ -4,4 +4,4 @@ task genffi, "update the nim ffi bindings":
   exec "nbindgen --crate codex-storage-proofs --output codex_proofs_ffi.nim"
 
 task tests, "run unit tests":
-  exec "testament pattern \"tests/t*.nim\" "
+  exec "nim c -r tests/tffi.nim"
--- tests/tffi.nim
+++ tests/tffi.nim
@@ -1,16 +1,19 @@
 
+import std/os
 import unittest2
 import codex_storage_proofs
 
 suite "storage proofs ffi":
   test "basic ffi circuit":
     var
-      r1cs_path = "src/circuit_tests/artifacts/storer-test.r1cs"
-      wasm_path = "src/circuit_tests/artifacts/storer-test_js/storer-test.wasm"
+      r1csPath = "src/circuit_tests/artifacts/storer-test.r1cs".absolutePath()
+      wasmPath = "src/circuit_tests/artifacts/storer-test_js/storer-test.wasm".absolutePath()
 
+    assert r1csPath.fileExists()
+    assert wasmPath.fileExists()
     let
-      r1cs_buff = unsafeBufferPath(r1cs_path)
-      wasm_buff = unsafeBufferPath(wasm_path)
+      r1cs_buff = unsafeBufferPath(r1csPath)
+      wasm_buff = unsafeBufferPath(wasmPath)
 
     let storage_ctx = init_storage_proofs(r1cs_buff, wasm_buff, nil)
 
@@ -18,7 +21,7 @@ suite "storage proofs ffi":
     check storage_ctx != nil
 
     var
-      mpack_arg_path = "test/proof_test.mpack"
+      mpack_arg_path = "tests/proof_test.mpack"
       proofBuff = unsafeBufferFromFile(mpack_arg_path)
     echo "proofArgs:size: ", proofBuff.len()
     let res = prove_mpack_ext(storage_ctx, addr proofBuff)
